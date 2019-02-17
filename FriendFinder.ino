857ceb66cd7505b6ff4d3e2f09f7b34f7fc2d203
cmcoffman <clayton.coffman@gmail.com>
2019-02-17 17:46:00 -0600

    tidyup

--- FriendFinder.ino
+++ FriendFinder.ino
@@ -2,15 +2,14 @@
 #include "FriendFinder.h"
 
 // Set Ring size
-#define SMALL_NEORING_SIZE 24
-// Strip is connected to Arduino Pin 1
-#define STRIP_PIN 5
-NeoDisplay ring = NeoDisplay(SMALL_NEORING_SIZE, STRIP_PIN, NEO_GRB + NEO_KHZ800);
+#define NUMPIXELS 24
+// Strip is connected to Arduino Pin 5
+#define NEOPIXEL_RING_PIN 5
+NeoDisplay ring = NeoDisplay(NUMPIXELS, NEOPIXEL_RING_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);
-  delay(500);  // delay a bit when we start so we can open arduino serial
-               // monitor window
+  delay(500);
   Serial.println("Starting NeoEffects Test");
   ring.begin();
   ring.clearStrip();
@@ -20,7 +19,6 @@ void setup() {
 void loop() {
   delay(100);
   if (millis() < 10000) {
-    // ring.fillStrip(ring.randomColor());
     ring.colorDotWipe(ring.Blue, 50);
     ring.colorWipe(ring.randomColor(), 50);
     ring.colorDot(1, ring.Red);
