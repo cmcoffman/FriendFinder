06569a4f454381657167b0f775c75821ded6fac7
cmcoffman <clayton.coffman@gmail.com>
2022-06-28 13:19:33 -0500

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -1,2 +1,3 @@
-#define WIFI_SSID "***REMOVED***" // name of your WiFi network
-#define WIFI_PASSWORD "***REMOVED***" // password for your WiFi network
+#define WIFI_SSID "WIFI_NETWORK_NAME" // name of your WiFi network
+#define WIFI_PASSWORD "WIFI_NETWORK_PASSWORD" // password for your WiFi network
+ 
